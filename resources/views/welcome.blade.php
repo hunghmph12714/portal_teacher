8da6f606e7934a5fdac7f89dfe04c45ebc35e3cf
thanhttb <tranthanhuet@gmail.com>
2020-09-10 11:35:53 +0700

    no cache

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,9 @@
         <meta name="viewport" content="width=device-width, minimal-ui, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"/>
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-title" content="VEE">
+        <meta http-equiv='cache-control' content='no-cache'>
+<meta http-equiv='expires' content='0'>
+<meta http-equiv='pragma' content='no-cache'>
         <title>VieElite Education</title>
 
         <!-- Fonts -->
