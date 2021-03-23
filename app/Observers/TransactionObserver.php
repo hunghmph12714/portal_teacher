b9a182612835319738bca346cdd1d1b2b4f022a7
thanhttb <tranthanhuet@gmail.com>
2021-03-23 14:55:20 +0700

    ace

--- app/Observers/TransactionObserver.php
+++ app/Observers/TransactionObserver.php
@@ -70,16 +70,31 @@ class TransactionObserver
         $old_debit = $transaction->getOriginal('debit');
         $old_credit = $transaction->getOriginal('credit');
         $old_amount = $transaction->getOriginal('amount');
-        $debit = Account::find($old_debit);
-        $credit = Account::find($old_credit);
-        if($old_debit != $transaction->debit){            
-            $debit->balance += $old_amount;
-            $debit->save();
-            $new_debit = Account::find($transaction->debit);
-            $new_debit->balance -= $transaction->amount;
-            $new_debit->save();
-            
-            if($old_credit != $transaction->credit){                
+        
+        if(!$old_debit && !$old_credit){
+            $debit = Account::find($old_debit);
+            $credit = Account::find($old_credit);
+            if($old_debit != $transaction->debit){            
+                $debit->balance += $old_amount;
+                $debit->save();
+                $new_debit = Account::find($transaction->debit);
+                $new_debit->balance -= $transaction->amount;
+                $new_debit->save();
+                
+                if($old_credit != $transaction->credit){                
+                    $credit->balance -= $old_amount;                
+                    $credit->save();
+                    
+                    $new_credit = Account::find($transaction->credit);
+                    $new_credit->balance += $transaction->amount;
+                    $new_credit->save();
+                }
+                else{
+                    $credit = Account::find($transaction->credit);
+                    $credit->balance = $credit->balance - $old_amount + $transaction->amount;
+                }
+            }
+            if($old_debit == $transaction->debit && $old_credit != $transaction->credit){
                 $credit->balance -= $old_amount;                
                 $credit->save();
                 
@@ -87,46 +102,35 @@ class TransactionObserver
                 $new_credit->balance += $transaction->amount;
                 $new_credit->save();
             }
-            else{
-                $credit = Account::find($transaction->credit);
+            if($old_debit == $transaction->debit && $old_credit == $transaction->credit && $old_amount != $transaction->amount){
+                $debit->balance = $debit->balance + $old_amount - $transaction->amount;
                 $credit->balance = $credit->balance - $old_amount + $transaction->amount;
+                $debit->save(); $credit->save();
             }
-        }
-        if($old_debit == $transaction->debit && $old_credit != $transaction->credit){
-            $credit->balance -= $old_amount;                
-            $credit->save();
-            
-            $new_credit = Account::find($transaction->credit);
-            $new_credit->balance += $transaction->amount;
-            $new_credit->save();
-        }
-        if($old_debit == $transaction->debit && $old_credit == $transaction->credit && $old_amount != $transaction->amount){
-            $debit->balance = $debit->balance + $old_amount - $transaction->amount;
-            $credit->balance = $credit->balance - $old_amount + $transaction->amount;
-            $debit->save(); $credit->save();
-        }
-        if($transaction->budget_id){
-            $old_budget = $transaction->getOriginal('budget_id');
-            if($old_budget){
-                $old_ba = BudgetAccount::where('budget_id', $old_budget)->where('account_id', $transaction->getOriginal('debit'))->first();
-                if($old_ba){
-                    $old_ba->actual -= $transaction->getOriginal('amount');
-                    $old_ba->save();
+            if($transaction->budget_id){
+                $old_budget = $transaction->getOriginal('budget_id');
+                if($old_budget){
+                    $old_ba = BudgetAccount::where('budget_id', $old_budget)->where('account_id', $transaction->getOriginal('debit'))->first();
+                    if($old_ba){
+                        $old_ba->actual -= $transaction->getOriginal('amount');
+                        $old_ba->save();
+                    }
                 }
+                //update new budget 
+                $ba = BudgetAccount::where('budget_id', $transaction->budget_id)->where('account_id', $transaction->debit)->first();
+                if($ba){
+                    $ba->actual += $transaction->amount;
+                    $ba->save();
+                }else{
+                    $input['budget_id'] = $transaction->budget_id;
+                    $input['account_id'] = $transaction->debit;
+                    $input['limit'] = 0;
+                    $input['actual'] = $transaction->amount;
+                    BudgetAccount::create($input);
+                } 
             }
-            //update new budget 
-            $ba = BudgetAccount::where('budget_id', $transaction->budget_id)->where('account_id', $transaction->debit)->first();
-            if($ba){
-                $ba->actual += $transaction->amount;
-                $ba->save();
-            }else{
-                $input['budget_id'] = $transaction->budget_id;
-                $input['account_id'] = $transaction->debit;
-                $input['limit'] = 0;
-                $input['actual'] = $transaction->amount;
-                BudgetAccount::create($input);
-            } 
         }
+        
     }
 
     /**
