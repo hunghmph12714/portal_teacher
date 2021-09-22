15b2a7d2d9721b105cf66be64cf8ae48f9b55be2
Tran Thanh <tranthanh@MacBook-Air-cua-Tran.local>
2021-09-22 17:04:33 +0700

    transaction filter 2

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -365,6 +365,8 @@ class TransactionController extends Controller
                 ->transactionDebit($request->filter)
                 ->transactionCredit($request->filter)
                 ->transactionTag($request->filter);
+            $result['total'] = $transactions->count();
+
             $transactions = $transactions->Select(
                 'transactions.id as id','transactions.amount' ,DB::raw("DATE_FORMAT(transactions.time, '%d/%m/%Y') as time_formated"),'transactions.time','transactions.content','transactions.created_at',
                 'debit_account.id as debit_id','debit_account.level_2 as debit_level_2', 'debit_account.name as debit_name', 'debit_account.type as debit_type',
@@ -383,7 +385,6 @@ class TransactionController extends Controller
                 ->leftJoin('sessions', 'transactions.session_id','sessions.id')
                 ->leftJoin('users', 'transactions.user', 'users.id')->orderBy('transactions.created_at', 'DESC')->offset($offset)->limit($request->per_page)
                 ->get();
-            
         }
         if($transactions){
             $x = $transactions->toArray();
