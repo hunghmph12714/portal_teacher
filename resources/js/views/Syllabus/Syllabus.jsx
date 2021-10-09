551ebf9b89755bc568b72a7a42b86e72ac54edf7
Tran Thanh <tranthanh@MacBook-Air-cua-Tran.local>
2021-10-09 11:56:57 +0700

    acbei

--- resources/js/views/Syllabus/Syllabus.jsx
+++ resources/js/views/Syllabus/Syllabus.jsx
@@ -31,21 +31,13 @@ import { PopoverCloseButton } from '@chakra-ui/core';
 // }
 var id_global = 0;
 const Syllabus = (props) => {
-    const [syllabus, setSyllabus] = useState({
-        title: '',
-        grade: '',
-        subject: '',
-        description: '',
-        subject: '',
-        public: true,
-        id: null,
-    })
+    const [syllabus, setSyllabus] = useState(null)
     const [current, setCurrent] = useState({chapter: '', subject: ''})
     const [chapters, setChapters] = useState([])
     const [mode, setMode] = useState('create')
     const {enqueueSnackbar} = useSnackbar()
     useEffect(()=> {
-        if(props.match.params.id){
+        if(props.match.params.id && props.match.params.id !== 'tao-moi'){
             
             async function fetchData(){
                 let response = await axios.post('/syllabus/get', {id: props.match.params.id})
@@ -62,6 +54,14 @@ const Syllabus = (props) => {
                 setMode('edit')
             }
             fetchData()
+        }else{
+            setSyllabus({
+                title: '',
+                grade: '',
+                subject: '',
+                public: false,
+                id: null,
+            })
         }
     }, [])
     function onChange(event){
@@ -212,108 +212,112 @@ const Syllabus = (props) => {
         <div className="syllabus-root">
             <Grid container spacing={2}>
                 <Grid item md={9}>
-                    <Paper>
-                        <div className="syllabus-content">
-                            {mode == "create" ? <h4>Tạo mới khoá học</h4> : <h4>Chỉnh sửa khoá học </h4>}
-                            <TextField 
-                                className="syllabus-title"
-                                label="Tên khoá học" 
-                                variant="outlined" 
-                                size="small"
-                                name="title"
-                                fullWidth
-                                value = {syllabus.title}
-                                onChange = {onChange}
-                            />
-                            <Grid container spacing={2}>
-                                <Grid item md={4}> 
-                                    <FormControl variant="outlined" size="small" fullWidth>
-                                        <InputLabel htmlFor="grade">Khối(*)</InputLabel>
-                                        <Select
-                                            native
-                                            value={syllabus.grade}
-                                            onChange={onChange}
-                                            name="grade"
-                                            label="Khối(*)"
-                                        >
-                                            <option aria-label="None" value="" />
-                                            <option value={1}>Lớp 1</option>
-                                            <option value={2}>Lớp 2</option>
-                                            <option value={3}>Lớp 3</option>
-                                            <option value={4}>Lớp 4</option>
-                                            <option value={5}>Lớp 5</option>
-                                            <option value={6}>Lớp 6</option>
-                                            <option value={7}>Lớp 7</option>
-                                            <option value={8}>Lớp 8</option>
-                                            <option value={9}>Lớp 9</option>
-                                            <option value={10}>Lớp 10</option>
-                                            <option value={11}>Lớp 11</option>
-                                            <option value={12}>Lớp 12</option>
-                                        </Select>
-                                    </FormControl>
+                    {syllabus ? (
+                        <Paper>
+                            <div className="syllabus-content">
+                                {mode == "create" ? <h4>Tạo mới khoá học</h4> : <h4>Chỉnh sửa khoá học </h4>}
+                                <TextField 
+                                    className="syllabus-title"
+                                    label="Tên khoá học" 
+                                    variant="outlined" 
+                                    size="small"
+                                    name="title"
+                                    fullWidth
+                                    value = {syllabus.title}
+                                    onChange = {onChange}
+                                />
+                                <Grid container spacing={2}>
+                                    <Grid item md={4}> 
+                                        <FormControl variant="outlined" size="small" fullWidth>
+                                            <InputLabel htmlFor="grade">Khối(*)</InputLabel>
+                                            <Select
+                                                native
+                                                value={syllabus.grade}
+                                                onChange={onChange}
+                                                name="grade"
+                                                label="Khối(*)"
+                                            >
+                                                <option aria-label="None" value="" />
+                                                <option value={1}>Lớp 1</option>
+                                                <option value={2}>Lớp 2</option>
+                                                <option value={3}>Lớp 3</option>
+                                                <option value={4}>Lớp 4</option>
+                                                <option value={5}>Lớp 5</option>
+                                                <option value={6}>Lớp 6</option>
+                                                <option value={7}>Lớp 7</option>
+                                                <option value={8}>Lớp 8</option>
+                                                <option value={9}>Lớp 9</option>
+                                                <option value={10}>Lớp 10</option>
+                                                <option value={11}>Lớp 11</option>
+                                                <option value={12}>Lớp 12</option>
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                    <Grid item md={4}>
+                                        <FormControl variant="outlined" size="small" fullWidth>
+                                            <InputLabel htmlFor="grade">Bộ môn(*)</InputLabel>
+                                            <Select
+                                                native
+                                                value={syllabus.subject}
+                                                onChange={onChange}
+                                                name="subject"
+                                                label="Bộ môn(*)"
+                                            >
+                                                <option aria-label="None" value="" />
+                                                <option value={'Toán'}>Toán </option>
+                                                <option value={'Tiếng Việt'}>Tiếng Việt</option>
+                                                <option value={'Tiếng Anh'}>Tiếng Anh</option>
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                    <Grid item md={4}>
+                                        <FormGroup>
+                                            <FormControlLabel control={
+                                                <Switch
+                                                    checked={syllabus.public}
+                                                    onChange={onPublicChange}
+                                                />} 
+                                                label="Công khai khoá học" 
+                                            />
+                                        </FormGroup>
+                                    </Grid>
                                 </Grid>
-                                <Grid item md={4}>
-                                    <FormControl variant="outlined" size="small" fullWidth>
-                                        <InputLabel htmlFor="grade">Bộ môn(*)</InputLabel>
-                                        <Select
-                                            native
-                                            value={syllabus.subject}
-                                            onChange={onChange}
-                                            name="subject"
-                                            label="Bộ môn(*)"
-                                        >
-                                            <option aria-label="None" value="" />
-                                            <option value={'Toán'}>Toán </option>
-                                            <option value={'Tiếng Việt'}>Tiếng Việt</option>
-                                            <option value={'Tiếng Anh'}>Tiếng Anh</option>
-                                        </Select>
-                                    </FormControl>
-                                </Grid>
-                                <Grid item md={4}>
-                                    <FormGroup>
-                                        <FormControlLabel control={
-                                            <Switch
-                                                checked={syllabus.public}
-                                                onChange={onPublicChange}
-                                            />} 
-                                            label="Công khai khoá học" 
-                                        />
-                                    </FormGroup>
-                                </Grid>
-                            </Grid>
-                            <CKEditor
-                                editor={ClassicEditor}
-                                config={{
-                                    toolbar: {
-                                        items: [
-                                            'heading',
-                                            '|',
-                                            'bold',
-                                            'italic',
-                                            'link',
-                                            'bulletedList',
-                                            'numberedList',
-                                            'imageUpload',
-                                            'mediaEmbed',
-                                            'insertTable',
-                                            'blockQuote',
-                                            'undo',
-                                            'redo'
-                                        ]
-                                    },
+                                <CKEditor
+                                    editor={ClassicEditor}
+                                    config={{
+                                        toolbar: {
+                                            items: [
+                                                'heading',
+                                                '|',
+                                                'bold',
+                                                'italic',
+                                                'link',
+                                                'bulletedList',
+                                                'numberedList',
+                                                'imageUpload',
+                                                'mediaEmbed',
+                                                'insertTable',
+                                                'blockQuote',
+                                                'undo',
+                                                'redo'
+                                            ]
+                                        },
+                                        }}
+                                    data= {(syllabus.description) ? syllabus.description : ''}
+                                    onReady={editor => {
+                                        // You can store the "editor" and use when it is needed.
+                                        // console.log( 'Editor is ready to use!', editor );
+                                        }}
+                                    onChange={ ( event, editor ) => {
+                                        setSyllabus({...syllabus, description: editor.getData()})
                                     }}
-                                data= {(syllabus.description) ? syllabus.description : ''}
-                                onReady={editor => {
-                                    // You can store the "editor" and use when it is needed.
-                                    // console.log( 'Editor is ready to use!', editor );
-                                    }}
-                                onChange={ ( event, editor ) => {
-                                    setSyllabus({...syllabus, description: editor.getData()})
-                                }}
-                            />
-                        </div>
-                        
-                    </Paper> 
+                                />
+                            </div>
+                            
+                        </Paper>
+                    ):(
+                        ''
+                    )}
                     {chapters.map((c, index) => 
                         <Accordion>
                             <AccordionSummary
