e19a233d583f7487aa1be6f744fabbaae880dbd7
hunghmph12714 <84831634+hunghmph12714@users.noreply.github.com>
2022-07-16 08:42:51 +0700

    update export entrance

--- app/Http/Controllers/CareController.php
+++ app/Http/Controllers/CareController.php
@@ -10,6 +10,8 @@ use App\Exports\CareExport;
 use App\Service;
 // use App\CareServive;
 use App\Student;
+use App\StudentClass;
+use App\UserClass;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Facades\Excel;
@@ -53,7 +55,7 @@ class CareController extends Controller
     public function list(Request $request)
     {
         // dd(1);
-        $class_id=$request->class_id;
+        $class_id = $request->class_id;
         $care_services =   Care::query()->where('class_id', $class_id)->orderBy('id', 'desc')->get();
         $care_services->load('care_service');
         $care_services->load('student');
@@ -75,10 +77,10 @@ class CareController extends Controller
 
 
             // ];
-        $care['care_services']=[];
+            $care['care_services'] = [];
             foreach ($c->care_service as $cs) {
                 // 
-            
+
                 $a = [
                     // 'care_id' => $cs->care_id,
                     'service_id' => $cs->service->id,
@@ -87,7 +89,7 @@ class CareController extends Controller
 
 
                 ];
-                array_push($care['care_services'],$a);
+                array_push($care['care_services'], $a);
             }
 
             array_push($cares, $care);
@@ -123,7 +125,7 @@ class CareController extends Controller
     }
     public function exportCare(Request $request)
     {
-        $class_id=$request->class_id;
+        $class_id = $request->class_id;
         $care_services =   Care::query()->where('class_id', $class_id)->orderBy('id', 'desc')->get();
         $care_services->load('care_service');
         $care_services->load('student');
@@ -136,12 +138,12 @@ class CareController extends Controller
             $care['student'] = ['id' => $c->student_id, 'name' => $c->student->fullname];
             $care['user'] = ['id' => $c->user_id, 'name' => $c->user->name];
             $care['time'] = ['created_at' => date('d/m/Y', strtotime($c->created_at)), 'updated_at' => $c->created_at];
-            $care['method']=$c->method;
-            $care['class']=['code'=>$c->class->code,'name'=>$c->class->name];
-        $care['care_services']=[];
+            $care['method'] = $c->method;
+            $care['class'] = ['code' => $c->class->code, 'name' => $c->class->name];
+            $care['care_services'] = [];
             foreach ($c->care_service as $cs) {
                 // 
-            
+
                 $a = [
                     // 'care_id' => $cs->care_id,
                     'service_id' => $cs->service->id,
@@ -150,13 +152,46 @@ class CareController extends Controller
 
 
                 ];
-                array_push($care['care_services'],$a);
+                array_push($care['care_services'], $a);
             }
 
             array_push($cares, $care);
         }
-        $data['cares']=$cares;
-        $data['services']=Service::where('active',1)->select('name')->get()->toArray();
-        return  Excel::download(new CareExport($data),'cares.xlsx');
+        $data['cares'] = $cares;
+        $data['services'] = Service::where('active', 1)->select('name')->get()->toArray();
+        return  Excel::download(new CareExport($data), 'cares.xlsx');
+    }
+    public function StatisticByUsers(Request $request)
+    {
+        $users = $request->user;
+        $from = date('d/m/Y', strtotime($request->from));
+        $to = date('d/m/Y', strtotime($request->to));
+        // dd($request);
+        // Số lớp được phân
+        $classes = UserClass::where('role', 2)->whereIn('user_id', $users)->join('classes', 'user_class.class_id', 'classes.id')->groupBy('classes.id')->select('classes.id as id');
+
+        //Số lớp đã chăm sóc
+        $class_care = Care::whereBetween('created_at', [$from, $to])->groupBy('class_id')->whereIn('user_id', [$users])->count();
+
+        // Số lượt học ở các lớp được phân
+        $student_session = UserClass::where('role', 2)->whereIn('user_class.user_id', $users)
+        // ->join('classes', 'user_class.class_id', 'classes.id')
+        // ->groupBy('classes.id')
+        ->join('sessions', 'user_class.class_id', 'sessions.class_id')
+        ->join('student_session', 'sessions.id', 'student_session.session_id')->count();
+
+        // số lượt chăm sóc
+        $students_care = Care::whereBetween('created_at', [$from, $to])->groupBy('id')->whereIn('user_id', [$users]);
+
+        // $Tổng số  'LƯỢT' học sinh phải chăm sóc
+        $arr_class = array_column($classes->get()->toArray(), 'id');
+        $students = StudentClass::whereIn('class_id', $arr_class)->count();
+
+        // Tổng số lượt học sinh đã chăm sóc
+
+        $students_after_care = $students_care->orderBy('student_id')->count();
+
+
+        dd($classes->count(), $student_session, $class_care, $students, $students_care->count() ,$students_after_care );
     }
 }
